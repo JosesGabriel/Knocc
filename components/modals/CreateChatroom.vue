670247390328482db726bc041936a14b5bd1c587
Joses Gabriel Lu <josesgabriellu@gmail.com>
2020-03-10 11:20:31 +0800

    Create chat room

--- components/modals/CreateChatroom.vue
+++ components/modals/CreateChatroom.vue
@@ -10,6 +10,7 @@
       <v-row>
         <v-col cols="12" class="pr-1">
           <v-text-field
+            v-model="roomName"
             hide-details
             outlined
             placeholder="Name"
@@ -20,6 +21,7 @@
       <v-row>
         <v-col cols="12" class="pr-1 py-0">
           <v-text-field
+            v-model="roomTopic"
             hide-details
             outlined
             placeholder="Topic (optional)"
@@ -35,7 +37,7 @@
           </div>
         </v-col>
         <v-col cols="2">
-          <v-switch color="success" inset></v-switch>
+          <v-switch v-model="roomIsPublic" color="success" inset></v-switch>
         </v-col>
       </v-row>
     </v-container>
@@ -52,6 +54,7 @@
       <v-btn
         color="success"
         class="black--text no-transform px-7 font-weight-bold"
+        @click="createRoom()"
       >
         Create Room
       </v-btn>
@@ -60,10 +63,36 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import { client } from "~/assets/client.js";
 export default {
   components: {},
   data() {
-    return {};
+    return {
+      roomName: "",
+      roomTopic: "",
+      roomIsPublic: false
+    };
+  },
+  computed: {
+    ...mapGetters({
+      clientIsPrepared: "global/getClientIsPrepared"
+    })
+  },
+  methods: {
+    createRoom() {
+      if (this.clientIsPrepared) {
+        const options = {
+          name: this.roomName,
+          visibility: this.roomIsPublic ? "public" : "private",
+          room_alias_name: this.roomTopic
+        };
+        client.createRoom(options).then(response => {
+          this.$emit("close");
+          console.log(response);
+        });
+      }
+    }
   }
 };
 </script>
