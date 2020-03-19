eb82a034da61443da6ce6cbd5daf89b724f0358e
Joses Gabriel Lu <josesgabriellu@gmail.com>
2020-03-19 15:23:40 +0800

    send private chat

--- components/modals/StartChat.vue
+++ components/modals/StartChat.vue
@@ -16,11 +16,18 @@
       </v-row>
       <v-row>
         <v-col cols="12" class="pr-1">
-          <v-text-field
-            outlined
+          <v-autocomplete
+            v-model="selected"
+            :items="userSuggestions"
+            item-text="display_name"
+            item-value="user_id"
+            :search-input.sync="search"
             placeholder="Email, Name or Username"
+            chips
+            outlined
             dense
-          ></v-text-field>
+            dark
+          ></v-autocomplete>
         </v-col>
       </v-row>
     </v-container>
@@ -38,6 +45,7 @@
       <v-btn
         color="success"
         class="black--text no-transform px-7 font-weight-bold"
+        @click="startChat()"
       >
         Start Chat
       </v-btn>
@@ -46,10 +54,52 @@
 </template>
 
 <script>
+import { mapGetters, mapActions } from "vuex";
+import { client } from "~/assets/client.js";
 export default {
   components: {},
   data() {
-    return {};
+    return {
+      userSuggestions: [],
+      selected: [],
+      search: null
+    };
+  },
+  computed: {
+    ...mapGetters({
+      clientIsPrepared: "global/getClientIsPrepared",
+      currentRoom: "global/getCurrentRoom"
+    })
+  },
+  watch: {
+    search(e) {
+      const params = {
+        search_term: e
+      };
+      this.$api.search.create(params).then(response => {
+        this.userSuggestions = response.results;
+      });
+    }
+  },
+  methods: {
+    ...mapActions({
+      setAlert: "global/setAlert"
+    }),
+    startChat() {
+      if (this.clientIsPrepared) {
+        let invitees = [];
+        invitees.push(this.selected);
+        let options = {
+          preset: "trusted_private_chat",
+          visibility: "private",
+          invite: invitees,
+          is_direct: true
+        };
+        client.createRoom(options).then(response => {
+          this.$emit("close");
+        });
+      }
+    }
   }
 };
 </script>
