512f3b56c1e70bd9b9866881801fe710f3d31182
nick <nickgayo12@gmail.com>
2020-04-01 20:38:09 +0800

    Replace col loop to list loop

--- components/main/RoomMembers.vue
+++ components/main/RoomMembers.vue
@@ -1,79 +1,59 @@
 <template>
   <v-container class="pa-0 ma-0">
-    <v-row no-gutters>
-      <v-col cols="12" class="px-6">
-        <v-btn
-          block
-          color="success"
-          class="black--text"
-          @click="inviteToRoomsModal = !inviteToRoomsModal"
-        >
-          <v-icon>mdi-account-plus-outline</v-icon>
-          <span class="no-transform font-weight-black"
-            >Invite to this room</span
-          >
-        </v-btn>
-      </v-col>
-    </v-row>
-    <div>
-      <v-row
-        v-for="member in roomMembers"
-        :key="member.userId"
-        class="roomMember__item"
-        no-gutters
+    <div class="d-block pa-3">
+      <v-btn
+        block
+        color="success"
+        class="black--text"
+        @click="inviteToRoomsModal = !inviteToRoomsModal"
       >
-        <v-col cols="2" class="pa-0">
-          <v-avatar class="mx-auto d-block" size="30">
-            <v-img :src="member.avatarUrl"></v-img>
-          </v-avatar>
-        </v-col>
-        <v-col cols="8" class="pa-0 pt-1 pl-2">
-          <span>{{ member.displayName }}</span>
-        </v-col>
-        <v-col cols="2" class="pt-1">
-          <v-btn icon x-small class="">
-            <v-icon>
-              mdi-chevron-right
-            </v-icon>
-          </v-btn>
-        </v-col>
-      </v-row>
-      <v-row v-if="roomMembersLength > 30" class="roomMember__item">
-        <v-col cols="2" class="pa-0 pl-7">
-          <v-avatar size="30" color="white">
-            <v-icon color="black">mdi-dots-horizontal</v-icon>
-          </v-avatar>
-        </v-col>
-        <v-col cols="8" class="pa-0 pt-1 pl-2">
-          <i>and {{ roomMembersLength - 30 }} others...</i>
-        </v-col>
-      </v-row>
+        <v-icon>mdi-account-plus-outline</v-icon>
+        <span class="no-transform font-weight-black">Invite to this room</span>
+      </v-btn>
     </div>
-    <!-- <v-row>
-      <v-col cols="12" class="pt-0">
-        <span class="pl-4">INVITED</span>
-      </v-col>
-    </v-row>
-    <v-row v-for="invitedUser in invitedUsers" :key="invitedUser">
-      <v-col cols="2" class="pa-0 pl-7">
-        <v-avatar color="success" size="30">
-          <span class="white--text headline">J</span>
-        </v-avatar>
-      </v-col>
-      <v-col cols="8" class="pa-0 pt-1">
-        <span>{{ invitedUser }}</span>
-      </v-col>
-      <v-col cols="2" class="pt-1">
-        <v-btn icon x-small class="">
-          <v-icon>
-            mdi-chevron-right
-          </v-icon>
-        </v-btn>
-      </v-col>
-    </v-row> -->
+    <div>
+      <v-list class="pa-0" dense color="transparent">
+        <template v-for="(member, index) in roomMembers">
+          <v-hover v-slot:default="{ hover }">
+            <v-list-item :key="member.userId" @click>
+              <v-list-item-avatar size="30">
+                <v-img :src="member.avatarUrl"></v-img>
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <v-list-item-title class="caption" v-text="member.displayName"></v-list-item-title>
+              </v-list-item-content>
+
+              <v-list-item-action>
+                <v-btn icon x-small v-show="hover">
+                  <v-icon>mdi-chevron-right</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
+          </v-hover>
+        </template>
+
+        <v-list-item @click v-if="roomMembersLength > 30">
+          <v-list-item-avatar color="white" size="30">
+            <v-icon color="black">mdi-dots-horizontal</v-icon>
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>and {{ roomMembersLength - 30 }} others...</v-list-item-title>
+          </v-list-item-content>
+
+          <v-list-item-action>
+            <v-btn icon x-small>
+              <v-icon>mdi-chevron-right</v-icon>
+            </v-btn>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+    </div>
+
     <v-dialog v-model="inviteToRoomsModal" persistent max-width="740">
-      <InviteToRoom @close="inviteToRoomsModal = false"
-    /></v-dialog>
+      <InviteToRoom @close="inviteToRoomsModal = false" />
+    </v-dialog>
   </v-container>
 </template>
 <script>
